f4286e42fbe6b924f8874484f856f9935c2480d0
Adam Wisniewski <adamw@tbcn.ca>
2013-02-22 21:46:23 -0500

    starting on testing

---
+++ tests/test_dodo.py
@@ -0,0 +1,11 @@
+from unittest import TestCase
+
+class DodoTest(TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def returns_exception_on_error(self):
+        pass
